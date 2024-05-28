6f04b4b0c6c0b41a92b1c71ab9df58efa76f778f
Tal Harel <talharel80@gmail.com>
2024-05-28 10:07:31 +0300

    [frontend] tests infrastructure

--- frontend/tests/mswUtils/handlers.ts
+++ frontend/tests/mswUtils/handlers.ts
@@ -1,3 +1,26 @@
-import { http } from 'msw';
+import { log } from 'console';
+import { HttpResponse, http } from 'msw';
 
-export const handlers = [];
+export const handlers = [
+  http.get('http://localhost:8000/users/gender', () => {
+    return HttpResponse.json({
+      male: '60',
+      female: '40',
+    });
+  }),
+
+  http.get('http://localhost:8000/users/ages', async ({ request }) => {
+    const url = new URL(request.url);
+    const gap = parseInt(url.searchParams.get('gap')!);
+    const ages: Record<string, number> = {};
+    let start = 10;
+    let end = start + gap;
+
+    while (end < 100) {
+      ages[`${start}-${end}`] = start;
+      start += gap;
+      end += gap;
+    }
+    return HttpResponse.json(ages);
+  }),
+];
