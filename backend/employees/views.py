e4883787bf9a1966844d1a5122e978713ac2917e
Tal Harel <talharel80@gmail.com>
2024-06-21 12:00:12 +0300

    [backend] employees app Employe model

---
+++ backend/employees/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import EmployeeSerializer
+from .models import Employee
+
+
+
+@api_view(['POST'])
+def add_employee(request):
+    try:
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'error':str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def get_employees(request):
+    try:
+        employees = Employee.objects.all()
+        if employees.exists():
+            serializer = EmployeeSerializer(employees, many=True)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error':str(e)},status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+@api_view(['GET'])
+def get_employee(request):
+    try:
+        employees = Employee.objects.all()
+        if employees.exists():
+            serializer = EmployeeSerializer(employees, many=True)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error':str(e)},status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+
+@api_view(['GET'])
+def get_employee_by_id(request, id):
+    try:
+        employee = Employee.objects.get(id=id)
+    except Employee.DoesNotExist:
+        return Response({'error': 'Employee not found'}, status=status.HTTP_404_NOT_FOUND)
+    serializer = EmployeeSerializer(employee)
+    return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+def delete_employee_by_id(request, id):
+    try:
+        employee = Employee.objects.get(id=id)
+        employee.delete()
+        return Response({'message': 'Employee deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    except Employee.DoesNotExist:
+        return Response({'error': 'Employee not found'}, status=status.HTTP_404_NOT_FOUND)
--- backend/populate.py
+++ backend/populate.py
@@ -8,22 +8,57 @@ import random
 from faker import Faker
 from django.contrib.auth.models import User
 from users.models import UserProfile
+from employees.models import Employee
 
 fake = Faker()
 
+if UserProfile.objects.count() == 0:
+    for i in range(100):
 
-for i in range(100):
-
-    username = fake.user_name()
-    last_name = fake.last_name()
-    email = fake.email()
-    user = User.objects.create_user(username=username, last_name=last_name, email=email)
+        username = fake.user_name()
+        last_name = fake.last_name()
+        email = fake.email()
+        user = User.objects.create_user(username=username, last_name=last_name, email=email)
 
 
-    user_profile = UserProfile.objects.create(
-        user=user,
-        age=random.randint(12, 90),
-        gender=random.choice(['male', 'female'])
-    )
+        user_profile = UserProfile.objects.create(
+            user=user,
+            age=random.randint(12, 90),
+            gender=random.choice(['male', 'female'])
+        )
 
-print("Sample user profiles populated.")
+    print("Sample user profiles populated.")
+
+if Employee.objects.count() == 0:
+
+    positions = [
+    'Software Developer',
+    'Data Scientist',
+    'System Administrator',
+    'Network Engineer',
+    'Accountant',
+    'Economist',
+    'Investment Banker',
+    'Business Analyst',
+    'DevOps Engineer',
+    'UI/UX Designer',
+    ]
+
+    for i in range(40):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        email = fake.email()
+        age = random.randint(18, 65)
+        position = random.choice(positions)
+        hire_date = fake.date_between(start_date='-10y', end_date='today')
+
+        employee = Employee.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            age=age,
+            position=position,
+            hire_date=hire_date
+        )
+
+    print("Sample employees populated.")
