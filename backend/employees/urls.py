280cbe2853065aadc2b59f4f324b814b7692edf7
Tal Harel <talharel80@gmail.com>
2024-07-10 12:02:55 +0300

    [backend] celery implemention

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_employee,get_employees,get_employee_by_id,delete_employee_by_id
+from .views import add_employee,get_employees,get_employee_by_id,delete_employee_by_id,celery_func
 
 urlpatterns = [
     path('create',add_employee),
     path('',get_employees),
     path('<int:id>',get_employee_by_id),
-    path('delete/<int:id>',delete_employee_by_id)
+    path('delete/<int:id>',delete_employee_by_id),
+    path('celery', celery_func),
 ]
